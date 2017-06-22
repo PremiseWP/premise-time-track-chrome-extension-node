98f4f25efe90db0e8f2cfaa13ec4289564aed5a3
Mario Vallejo <mario@vallgroup.com>
2017-06-22 18:49:43 -0500

    new user buttons/profiile

--- src/UserButtons.js
+++ src/UserButtons.js
@@ -1,30 +1,73 @@
 import React, { Component } from 'react';
+import PTT from './PTT';
 import FontAwesome from 'react-fontawesome';
 
 class UserButtons extends Component {
   constructor(props) {
     super(props);
 
+    this._showOptions = this._showOptions.bind(this);
+    this._hideOptions = this._hideOptions.bind(this);
+
     this.state = {
       view: null,
+      options: '',
     };
   }
 
   componentWillMount() {
-    if (this.props.user && this.props.user.link) {
-      this.setState({
-        view: <div className="user-buttons-wrapper">
-          <a href={this.props.user.link}
-          target="_blank">
-            <FontAwesome className="user-profile" name="user" title="Visit Your Profile" />
-          </a>
-        </div>,
-      });
+    if (!this.props.user) {
+      return;
     }
   }
 
   render() {
-    return this.state.view;
+    const _userLink = (this.props.user.id)
+    ? PTT.get('site').url
+      + '/wp-admin/user-edit.php?user_id='
+      + this.props.user.id
+    : '';
+
+    const _avatar = (this.props.user.avatar_urls
+      && this.props.user.avatar_urls[24])
+    ? <img
+      src={this.props.user.avatar_urls[24]}
+      alt={this.props.user.name}
+      title="Visit Your Profile" />
+    : <FontAwesome
+      name="user"
+      title="Visit Your Profile" />;
+
+    return(
+      <div
+      className="user-buttons-wrapper"
+      onMouseEnter={this._showOptions.bind(this)}
+      onMouseLeave={this._hideOptions.bind(this)}>
+        {this.state.options}
+        <a href={_userLink}
+        target="_blank">
+          {_avatar}
+        </a>
+      </div>
+    );
+  }
+
+  _showOptions() {
+    this.setState({
+      options: <div
+      className="user-options">
+        You are logged in as:
+        <span className="user-name">
+          {this.props.user.name}
+        </span>
+      </div>,
+    });
+  }
+
+  _hideOptions() {
+    this.setState({
+      options: '',
+    });
   }
 }
 
