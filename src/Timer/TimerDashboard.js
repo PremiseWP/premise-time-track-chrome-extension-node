3d260852c62fccdf44594de3dadd7b7ff4475eb5
Mario Vallejo <mario@vallgroup.com>
2017-06-21 20:07:58 -0500

    dashboard now shows message

--- src/Timer/TimerDashboard.js
+++ src/Timer/TimerDashboard.js
@@ -1,24 +1,47 @@
 import React, { Component } from 'react';
 import TimerEditWidget from './TimerEditWidget';
 import TimerTaxonomyWidget from './TimerTaxonomyWidget';
+import FroceUserDownloadBtn from '../FroceUserDownloadBtn';
+import PTT from '../PTT';
+import HelpButtons from '../HelpButtons';
+import UserButtons from '../UserButtons';
+
 
 class TimerDashboard extends Component {
   constructor() {
-    super(); // super() must be called in our constructor.
+    super();
 
     this.state = {
-      timer: {}
+      message: '',
+      timer: PTT.get('current_timer') || {},
+      user: PTT.get('user') || {},
     };
   }
 
-  render() {
-    const timer = this.state.timer;
+  componentWillMount() {
+    if ( ! this.state.user.id ) {
+      this.setState({
+        message: <div className="message">
+          <p
+          className="error">
+            We seem to have a problem loading your user. Let us try and fix that.
+          </p>
+          <FroceUserDownloadBtn
+          onDownload={this._onUserForceDownload.bind(this)} />
+        </div>,
+      });
+    }
+  }
 
+  render() {
     return (
       <div className="timer-dashboard-wrapper">
-        <TimerEditWidget timer={timer} />
+        {this.state.message}
+        <TimerEditWidget />
         <TimerTaxonomyWidget taxonomyName="client" />
         <TimerTaxonomyWidget taxonomyName="project" />
+        <HelpButtons />
+        <UserButtons user={this.state.user} />
       </div>
     );
   }
@@ -30,6 +53,23 @@ class TimerDashboard extends Component {
     }
 
     this.setState({ timer });
+  }
+
+  _onUserForceDownload( user ) {
+    PTT.set(user, 'user');
+    PTT.setCookies();
+    this.setState({
+      message: <p>
+        {'Thank you! We\'ve got your user.'}
+      </p>,
+      user: PTT.get('user'),
+    });
+    const hideMsg = function() {
+      this.setState({
+        message: '',
+      });
+    };
+    setTimeout(hideMsg.bind(this), 3000);
   }
 }
 
